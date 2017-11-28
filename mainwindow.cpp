c8b7d328de907de1414a8f6238738a90103a46bc
javosmar <javosmar@gmail.com>
2017-11-28 20:29:49 -0300

    datos añadidos a base de datos, mostrados en tabla, latitud y longitud ajustados a formato decimal

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-bool estado_serial = false, conf = false, pedido = false, bandera = false, dato_valido = false;
-int x = 0;
+bool estado_serial = false, conf = false, pedido = false, bandera = false, dato_valido = false, ok;
+int contador = 0;
+QString validez, latitud, longitud, velocidad, pulsacion;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,21 +16,21 @@ MainWindow::MainWindow(QWidget *parent) :
             ui->serie_combo->insertItem(0,info.portName());
     }
     ui->serie_desconectar->setEnabled(false);
-//    //------SQL------
-//    qDebug() << "Iniciado";
-//    QString nombre;
-//    nombre.append("base_datos.sqlite");
-//    db = QSqlDatabase::addDatabase("QSQLITE");
-//    db.setDatabaseName(nombre);
-//    if(db.open()){
-//        qDebug() << "base de datos iniciada";
-//    }
-//    else{
-//        qDebug() << "error al abrir base de datos";
-//    }
-//    crearTablaUsuarios();
-//    mostrarDatos();
-//    //--------------
+    //------SQL------
+    qDebug() << "Iniciado";
+    QString nombre;
+    nombre.append("base_datos.sqlite");
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(nombre);
+    if(db.open()){
+        qDebug() << "base de datos iniciada";
+    }
+    else{
+        qDebug() << "error al abrir base de datos";
+    }
+    crearTablaUsuarios();
+    mostrarDatos();
+    //--------------
 }
 
 MainWindow::~MainWindow()
@@ -39,74 +40,74 @@ MainWindow::~MainWindow()
 
 void MainWindow::crearTablaUsuarios()
 {
-//    QString consulta;
-//    consulta.append("CREATE TABLE IF NOT EXISTS jugador("
-//                    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-//                    "valido VARCHAR(10),"
-//                    "latitud INTEGER NOT NULL,"
-//                    "longitud INTEGER NOT NULL,"
-//                    "velocidad INTEGER NOT NULL,"
-//                    "pulsacion INTEGER NOT NULL"
-//                    ");");
-//    QSqlQuery crear;
-//    crear.prepare(consulta);
-//    if(crear.exec()){
-//        qDebug() << "tabla creada correctamente";
-//    }
-//    else{
-//        qDebug() << "ERROR! " << crear.lastError();
-//    }
+    QString consulta;
+    consulta.append("CREATE TABLE IF NOT EXISTS jugador("
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    "validez VARCHAR(15),"
+                    "latitud INTEGER NOT NULL,"
+                    "longitud INTEGER NOT NULL,"
+                    "velocidad INTEGER NOT NULL,"
+                    "pulsacion VARCHAR(15)"
+                    ");");
+    QSqlQuery crear;
+    crear.prepare(consulta);
+    if(crear.exec()){
+        qDebug() << "tabla creada correctamente";
+    }
+    else{
+        qDebug() << "ERROR! " << crear.lastError();
+    }
 }
 
 void MainWindow::insertarUsuario()
 {
-//    QString consulta;
-//    consulta.append("INSERT INTO jugador("
-//                    "validez,"
-//                    "latitud,"
-//                    "longitud,"
-//                    "velocidad,"
-//                    "pulsacion)"
-//                    "VALUES("
-//                    "'"+ui->lineEditnombre->text()+"',"
-//                    ""+ui->lineEditapellido->text()+","
-//                    ""+ui->lineEditedad->text()+","
-//                    ""+ui->lineEditedad->text()+","
-//                    ""+ui->lineEditclase->text()+""
-//                    ");");
-//    QSqlQuery insertar;
-//    insertar.prepare(consulta);
-//    if(insertar.exec()){
-//        qDebug() << "usuario agregado correctamente";
-//    }
-//    else{
-//        qDebug() << "ERROR! " << insertar.lastError();
-//    }
+    QString consulta;
+    consulta.append("INSERT INTO jugador("
+                    "validez,"
+                    "latitud,"
+                    "longitud,"
+                    "velocidad,"
+                    "pulsacion) "
+                    "VALUES("
+                    "'"+validez+"',"
+                    ""+latitud+","
+                    ""+longitud+","
+                    ""+velocidad+","
+                    "'"+pulsacion+"'"
+                    ");");
+    QSqlQuery insertar;
+    insertar.prepare(consulta);
+    if(insertar.exec()){
+        qDebug() << "usuario agregado correctamente";
+    }
+    else{
+        qDebug() << "ERROR! " << insertar.lastError();
+    }
 }
 
 void MainWindow::mostrarDatos()
 {
-//    QString consulta;
-//    consulta.append("SELECT * FROM jugador");
-//    QSqlQuery mostrar;
-//    mostrar.prepare(consulta);
-//    if(mostrar.exec()){
-//        qDebug() << "consulta realizada con exito";
-//    }
-//    else{
-//        qDebug() << "ERROR! " << mostrar.lastError();
-//    }
-
-//    int fila = 0;
-//    ui->tableWidgetdato->setRowCount(0);
-//    while(mostrar.next()){
-//        ui->tableWidgetdato->insertRow(fila);
-//        ui->tableWidgetdato->setItem(fila,0,new QTableWidgetItem(mostrar.value(1).toByteArray().constData()));
-//        ui->tableWidgetdato->setItem(fila,1,new QTableWidgetItem(mostrar.value(2).toByteArray().constData()));
-//        ui->tableWidgetdato->setItem(fila,2,new QTableWidgetItem(mostrar.value(3).toByteArray().constData()));
-//        ui->tableWidgetdato->setItem(fila,3,new QTableWidgetItem(mostrar.value(4).toByteArray().constData()));
-//        fila++;
-//    }
+    QString consulta;
+    consulta.append("SELECT * FROM jugador");
+    QSqlQuery mostrar;
+    mostrar.prepare(consulta);
+    if(mostrar.exec()){
+        qDebug() << "consulta realizada con exito";
+    }
+    else{
+        qDebug() << "ERROR! " << mostrar.lastError();
+    }
+    int fila = 0;
+    ui->tableWidgetdato->setRowCount(0);
+    while(mostrar.next()){
+        ui->tableWidgetdato->insertRow(fila);
+        ui->tableWidgetdato->setItem(fila,0,new QTableWidgetItem(mostrar.value(1).toByteArray().constData()));
+        ui->tableWidgetdato->setItem(fila,1,new QTableWidgetItem(mostrar.value(2).toByteArray().constData()));
+        ui->tableWidgetdato->setItem(fila,2,new QTableWidgetItem(mostrar.value(3).toByteArray().constData()));
+        ui->tableWidgetdato->setItem(fila,3,new QTableWidgetItem(mostrar.value(4).toByteArray().constData()));
+        ui->tableWidgetdato->setItem(fila,4,new QTableWidgetItem(mostrar.value(5).toByteArray().constData()));
+        fila++;
+    }
 }
 
 void MainWindow::Serial_Conf()
@@ -127,6 +128,7 @@ void MainWindow::Serial_Conect()
         ui->serie_combo->setEnabled(false);
         ui->serie_actualizar->setEnabled(false);
         ui->serie_desconectar->setEnabled(true);
+        serial->write("A");
     }
     else
         Serial_Error();
@@ -134,6 +136,8 @@ void MainWindow::Serial_Conect()
 
 void MainWindow::Serial_Desconect()
 {
+    serial->write("X");
+    serial->waitForBytesWritten(30);
     serial->close();
     estado_serial = false;
     ui->serie_combo->setEnabled(true);
@@ -149,19 +153,20 @@ void MainWindow::Serial_Error()
     error.exec();
 }
 
-void MainWindow::Serial_Pedir()                         //muestreo de datos por pedido
+void MainWindow::Serial_Pedir()
 {
     if(serial->bytesAvailable() >= 28){
-        QString validez = serial->read(1);
-        QString latitud = serial->read(10);
-        QString longitud = serial->read(11);
-        QString velocidad = serial->read(5);
-        QString pulsacion = serial->read(1);
-        qDebug() << validez;
-        qDebug() << latitud;
-        qDebug() << longitud;
-        qDebug() << velocidad;
-        qDebug() << pulsacion;
+        validez = serial->read(1);
+        latitud = QString::number(serial->read(10).append("e-02").toDouble(&ok),'f',7);
+        longitud = QString::number(serial->read(11).append("e-02").toDouble(&ok),'f',7);
+        velocidad = serial->read(5);
+        pulsacion = serial->read(1);
+        insertarUsuario();
+        mostrarDatos();
+        contador++;
+        ui->progressBarserie->setValue(contador);
+        if(contador > 99)
+            contador = 0;
     }
 }
 
